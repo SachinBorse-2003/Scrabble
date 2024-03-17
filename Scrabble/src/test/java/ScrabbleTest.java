57a6adbdaba83c19557ae335596701033bdfa943
Sachin <Sachinkailasborse@gmail.com>
2024-03-17 09:10:51 +0530

    first commit

---
+++ Scrabble/src/test/java/ScrabbleTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ScrabbleTest {
+
+    @Test
+    public void shouldCreateInstance() {
+        Scrabble scrabble = new Scrabble();
+        assertEquals(0, scrabble.calculateScore(""));
+        
+    }
+}
