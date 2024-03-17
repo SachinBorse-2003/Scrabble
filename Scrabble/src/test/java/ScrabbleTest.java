addd5bbbe731a95ace9a1c03608619f51cad33fc
Sachin <Sachinkailasborse@gmail.com>
2024-03-17 21:23:28 +0530

    refactor test based on refoctored code

--- Scrabble/src/test/java/ScrabbleTest.java
+++ Scrabble/src/test/java/ScrabbleTest.java
@@ -1,13 +1,14 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ScrabbleTest {
 
     @Test
     public void shouldCreateInstance() {
         Scrabble scrabble = new Scrabble();
-        assertEquals(0, scrabble.calculateScore(""));
+       assertTrue(scrabble instanceof Scrabble);
     }
     @Test
     public void shouldThrowExceptionForEmptyWord() {
