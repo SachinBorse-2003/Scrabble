d151f4a9bef995806722c83776b1e897609613fb
Sachin <Sachinkailasborse@gmail.com>
2024-03-17 21:33:28 +0530

    Implement the main logic of the code as per the tests

--- Scrabble/src/main/java/Scrabble.java
+++ Scrabble/src/main/java/Scrabble.java
@@ -4,6 +4,20 @@ public class Scrabble {
         if (word == null || word.isEmpty()) {
             throw new IllegalArgumentException("Word cannot be null or empty");
         }
-        return 0;
+        
+        int score = 0;
+        for (char letter : word.toUpperCase().toCharArray()) {
+            switch (letter) {
+                case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' -> score += 1;
+                case 'D', 'G' -> score += 2;
+                case 'B', 'C', 'M', 'P' -> score += 3;
+                case 'F', 'H', 'V', 'W', 'Y' -> score += 4;
+                case 'K' -> score += 5;
+                case 'J', 'X' -> score += 8;
+                case 'Q', 'Z' -> score += 10;
+                default -> throw new IllegalArgumentException("Invalid character found in the word");
+            }
+        }
+        return score;
     }
 }
--- Scrabble/src/test/java/ScrabbleTest.java
+++ Scrabble/src/test/java/ScrabbleTest.java
@@ -17,4 +17,33 @@ public class ScrabbleTest {
             scrabble.calculateScore("");
         });
     }
+    @Test
+public void shouldCalculateScoreForSingleLetter() {
+    Scrabble scrabble = new Scrabble();
+    assertEquals(1, scrabble.calculateScore("A"));
+}
+@Test
+public void shouldCalculateScoreForWordWithMultipleLetters() {
+    Scrabble scrabble = new Scrabble();
+    assertEquals(8, scrabble.calculateScore("HELLO"));
+}
+@Test
+public void shouldCalculateScoreForWordWithMixedCase() {
+    Scrabble scrabble = new Scrabble();
+    assertEquals(14, scrabble.calculateScore("ScrAbBle"));
+}
+@Test
+public void shouldThrowExceptionForWordWithNonAlphabeticCharacters() {
+    Scrabble scrabble = new Scrabble();
+    assertThrows(IllegalArgumentException.class, () -> {
+        scrabble.calculateScore("123");
+    });
+}
+@Test
+public void shouldThrowExceptionForWordWithSpecialCharacters() {
+    Scrabble scrabble = new Scrabble();
+    assertThrows(IllegalArgumentException.class, () -> {
+        scrabble.calculateScore("!@#$%");
+    });
+}
 }
